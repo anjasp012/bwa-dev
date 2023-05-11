ccbaa6b59a13a7cf4aa87e8dd9a4209a0066a2c4
Anjasputra012 <anjasputra012@gmail.com>
2023-05-12 05:35:30 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -173,7 +173,7 @@
 @push('addon-script')
     <script>
         $('.owl-carousel').owlCarousel({
-            loop: true,
+            loop: false,
             margin: 10,
             nav: true,
             dots: false,
