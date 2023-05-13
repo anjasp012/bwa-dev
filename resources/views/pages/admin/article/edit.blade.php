851bb0b40ce3bbc14ccfd5be3b3e8b0bbf095661
Anjasputra012 <anjasputra012@gmail.com>
2023-05-14 05:40:41 +0700

    update

--- resources/views/pages/admin/article/edit.blade.php
+++ resources/views/pages/admin/article/edit.blade.php
@@ -46,7 +46,7 @@
                                     </div>
                                     <div class="row">
                                         <div class="col text-end">
-                                            <button class="btn btn-success px-5">Save Now</button>
+                                            <button type="submit" class="btn btn-success px-5">Save Now</button>
                                         </div>
                                     </div>
                                 </form>
